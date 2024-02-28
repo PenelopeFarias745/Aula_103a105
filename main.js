bbaf66f23221bf68ca0262b6c13a95fd4cbf211d
PenelopeFarias745 <156312541+PenelopeFarias745@users.noreply.github.com>
2024-02-28 18:59:09 -0300

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function takeSnapshot()
 {
     Webcam.snap(function(data_uri)
     {
-        document.getElementById("result").innerHMTL = '<img id="captured_image" src="'+data_uri+'"/>';
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
 
     });
 }
