86f8a5e9d499dbdb80a91f22918d988542e95b72
PenelopeFarias745 <156312541+PenelopeFarias745@users.noreply.github.com>
2024-02-28 18:52:57 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function takeSnapshot()
+{
+    Webcam.snap(function(data_uri)
+    {
+        document.getElementById("result").innerHMTL = '<img id="captured_image" src="'+data_uri+'"/>';
+
+    });
+}
+
+console.log('versão ml5:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/I4nwVKYtR/model.json', modelLoaded);
+
+function modelLoaded()
+{
+    console.log('Modelo carregado');
+}
+
